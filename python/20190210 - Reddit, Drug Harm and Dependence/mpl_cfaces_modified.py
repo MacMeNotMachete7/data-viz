dd04cc6849a279a1a8b97fb18614ea4d756c768b
Charles Lee <charleslee592@gmail.com>
2019-03-30 14:29:28 -0400

    create python directory

---
+++ python/20190210 - Reddit, Drug Harm and Dependence/mpl_cfaces_modified.py
@@ -0,0 +1,106 @@
+# https://gist.github.com/aflaxman/4043086
+
+'''
+ellipse over-lapping is not working
+but the faces look cuter without the ellipses...
+'''
+
+#from pylab import *
+
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
+import random
+#from mpl_toolkits.mplot3d import Axes3D
+
+def cface(ax, x1,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11,x12,x13,x14,x15,x16,x17,x18):
+    # x1 = height of upper face
+    # x2 = overlap of lower face
+    # x3 = half of vertical size of face
+    # x4 = width of upper face
+    # x5 = width of lower face
+    # x6 = length of nose
+    # x7 = vertical position of mouth
+    # x8 = curvature of mouth
+    # x9 = width of mouth
+    # x10 = vertical position of eyes
+    # x11 = separation of eyes
+    # x12 = slant of eyes
+    # x13 = eccentricity of eyes
+    # x14 = size of eyes
+    # x15 = position of pupils
+    # x16 = vertical position of eyebrows
+    # x17 = slant of eyebrows
+    # x18 = size of eyebrows
+    
+    # transform some values so that input between 0,1 yields variety of output
+    x3 = 1.9*(x3-.5)
+    x4 = (x4+.25)
+    x5 = (x5+.2)
+    x6 = .3*(x6+.01)
+    x8 = 5*(x8+.001)
+    x11 /= 5
+    x12 = 2*(x12-.5)
+    x13 += .05
+    x14 += .1
+    x15 = .5*(x15-.5)
+    x16 = .25*x16
+    x17 = .5*(x17-.5)
+    x18 = .5*(x18+.1)
+
+    # top of face, in box with l=-x4, r=x4, t=x1, b=x3
+    e = mpl.patches.Ellipse( (0,(x1+x3)/2), 2*x4, (x1-x3), fill=False, linewidth=2)
+    ax.add_artist(e)
+
+    # bottom of face, in box with l=-x5, r=x5, b=-x1, t=x2+x3
+    e = mpl.patches.Ellipse( (0,(-x1+x2+x3)/2), 2*x5, (x1+x2+x3), fill=False, linewidth=2)
+    ax.add_artist(e)
+
+    # cover overlaps
+    e = mpl.patches.Ellipse( (0,(x1+x3)/2), 2*x4, (x1-x3), fill=False, edgecolor='red')
+    ax.add_artist(e)
+    e = mpl.patches.Ellipse( (0,(-x1+x2+x3)/2), 2*x5, (x1+x2+x3), fill=False, edgecolor='green')
+    ax.add_artist(e)
+    
+#    # draw nose
+#    plt.plot([0,0], [-x6/2, x6/2], 'k')
+#    
+#    # draw mouth
+#    p = mpl.patches.Arc( (0,-x7+.5/x8), 1/x8, 1/x8, theta1=270-180/np.pi*np.arctan(x8*x9), theta2=270+180/np.pi*np.arctan(x8*x9))
+#    ax.add_artist(p)
+#    
+#    # draw eyes
+#    p = mpl.patches.Ellipse( (-x11-x14/2,x10), x14, x13*x14, angle=-180/np.pi*x12, fill=False)
+#    ax.add_artist(p)
+#    
+#    p = mpl.patches.Ellipse( (x11+x14/2,x10), x14, x13*x14, angle=180/np.pi*x12, fill=False)
+#    ax.add_artist(p)
+#
+#    # draw pupils
+#    p = mpl.patches.Ellipse( (-x11-x14/2-x15*x14/2, x10), .05, .05)
+#    ax.add_artist(p)
+#    p = mpl.patches.Ellipse( (x11+x14/2-x15*x14/2, x10), .05, .05)
+#    ax.add_artist(p)
+#    
+#    # draw eyebrows
+#    plt.plot([-x11-x14/2-x14*x18/2,-x11-x14/2+x14*x18/2],[x10+x13*x14*(x16+x17),x10+x13*x14*(x16-x17)],'k')
+#    plt.plot([x11+x14/2+x14*x18/2,x11+x14/2-x14*x18/2],[x10+x13*x14*(x16+x17),x10+x13*x14*(x16-x17)],'k')
+
+#fig = plt.figure(figsize=(11,11))
+fig, ax = plt.subplots(figsize=(10,10))
+#fig.set_figheight(15)
+#fig.set_figwidth(15)
+#fig.set_size_inches(10,10)
+#fig = plt.figure(figsize=(11,11))
+random.seed(1)
+for i in range(25):
+    ax = fig.add_subplot(5,5,i+1,aspect='equal')
+    
+    input = [random.random() for i in range(17)]
+#    print(input)
+    cface(ax, .9, *input)
+    ax.axis([-1.2,1.2,-1.2,1.2])
+    ax.set_xticks([])
+    ax.set_yticks([])
+
+fig.subplots_adjust(hspace=0, wspace=0)
